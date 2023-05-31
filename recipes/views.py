369e9ecbacfc89bd6fa47bccee497738e2bfc32a
Gleidson-Oliver <gleidsonoliver1998@gmail.com>
2023-05-30 22:23:23 -0300

    first update

--- recipes/views.py
+++ recipes/views.py
@@ -4,14 +4,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name': 'teste de exibição'
-    })
-
-
-def contato(request):
-    return HttpResponse('contato', )
-
-
-def sobre(request):
-    return HttpResponse('sobre')
+    return render(request, 'recipes/pages/home.html')
